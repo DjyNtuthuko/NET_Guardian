2fd8fb517760b1ca1ee3db77589346184efd18f3
Mike Ndaba <ST10444612@rcconnect.edu.za>
2026-05-26 23:42:00 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NET_Guardian
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
